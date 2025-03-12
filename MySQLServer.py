749df11fe8d0f06ab4295d932af83466e41e114b
Charity Thuo <charitynthuo@gmail.com>
2025-03-12 14:56:08 +0300

    Create a database and handle error messages.

---
+++ MySQLServer.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+# Replace with your connection details
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="Charity18",
+        password="xT9@bQ!4z&Wp7%Lm"
+)
+    if mydb.is_connected():
+        print("Connection successful!")
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error: {e}")
+
+finally:
+
+    if 'mydb' in locals() and mydb.is_connected():
+        mycursor.close()
+        mydb.close()
+        print("Database connection closed!")
+
+
